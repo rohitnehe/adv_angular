efa27758189101571cf17a20ff341ecde69e8703
Nikhil Bhagunde <nikhilbhagunde@benchmarkitsolutions.com>
2019-06-17 20:32:37 +0530

    add staging step

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  userServiceUrl: 'http://localhost/adv_users/api/web/v1/',
-  productServiceUrl: 'http://localhost/adv_products/api/web/v1/'
+  userServiceUrl: 'http://54.85.179.15/advuser/api/web/v1/',
+  productServiceUrl: 'http://54.85.179.15/advproduct/api/web/v1/'
 };
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    environmentName: 'staging',
+      userServiceUrl: 'http://18.206.158.245/advuser/api/web/v1/',
+  		productServiceUrl: 'http://18.206.158.245/advproduct/api/web/v1/'
+ };
