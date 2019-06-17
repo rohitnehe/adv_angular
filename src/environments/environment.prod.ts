4394d96845c17b7003f98703148e72439e3531fe
Ajay Khadke <ajaykhadke@benchmarkitsolutions.com>
2019-06-17 17:29:56 +0530

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  userServiceUrl: 'http://localhost/adv_users/api/web/v1/',
+  productServiceUrl: 'http://localhost/adv_products/api/web/v1/'
 };
