276cc538691f2ead2f1eec39478e9e9376658a77
Nikhil Bhagunde <nikhilbhagunde@benchmarkitsolutions.com>
2019-06-18 11:27:37 +0530

    working enviorment setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   userServiceUrl: 'http://54.85.179.15/advuser/api/web/v1/',
-  productServiceUrl: 'http://54.85.179.15/advproduct/api/web/v1/'
+  productServiceUrl: 'http://54.85.179.15/advproducts/api/web/v1/'
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     environmentName: 'staging',
       userServiceUrl: 'http://18.206.158.245/advuser/api/web/v1/',
-  		productServiceUrl: 'http://18.206.158.245/advproduct/api/web/v1/'
+  		productServiceUrl: 'http://18.206.158.245/advproducts/api/web/v1/'
  };
